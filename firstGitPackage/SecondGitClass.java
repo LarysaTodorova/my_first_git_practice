66cd813b179310f23ac810d5a037c8f19655afa2
Larysa Todorova <lorageo33@gmail.com>
2024-10-22 19:12:38 +0300

    git commit my second changed git file

--- firstGitPackage/SecondGitClass.java
+++ firstGitPackage/SecondGitClass.java
@@ -3,5 +3,6 @@ package firstGitPackage;
 public class SecondGitClass {
     public static void main(String[] args) {
         System.out.println("Hallow my second Git class!");
+        System.out.println("Second class was changed");
     }
 }
